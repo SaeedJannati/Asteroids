0ad23f61fa7c540f6ddd3c6b767907180dd21ae1
SaeedJannati <msaeedjannati72@gmail.com>
2021-12-09 15:33:40 +0330

    playerMovement

--- Assets/Asteriods/Core/SpaceShipMovement/Scripts/SpaceShipMevementModel.cs
+++ Assets/Asteriods/Core/SpaceShipMovement/Scripts/SpaceShipMevementModel.cs
@@ -1,8 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "SpaceShip Movement", menuName = "Asteriods/SpaceShip/Movement/Model")]
 public class SpaceShipMevementModel : ScriptableObject
 {
-  
+
+    #region Fields
+
+    public float maxSpeedX;
+    public float maxSpeedY;
+
+    public float yAcceleration;
+    public float xAcceleration;
+
+    #endregion
+
+    #region  Methods
+
+    
+    [Button]
+    public void PrintJsonToConsole()
+    {
+        Debug.Log(JsonConvert.SerializeObject(this));
+    }
+    #endregion
+    
+   
 }
--- Assets/Asteriods/Core/SpaceShipMovement/Scripts/SpaceShipMovementLogic.cs
+++ Assets/Asteriods/Core/SpaceShipMovement/Scripts/SpaceShipMovementLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +8,48 @@ public class SpaceShipMovementLogic : MonoBehaviour
    #region  Fields
 
    private SpaceShipMevementModel config;
+   private Vector3 currentSpeed=new Vector3();
 
+   private Transform mTrtansform=null;
 
+   private Vector3 position=new Vector3();
    #endregion
 
    #region Monbehaviour callbacks
 
-
+   private void Awake()
+   {
+      mTrtansform = GetComponent<Transform>();
+   }
 
    #endregion
 
    #region Methods
 
 
+   public void ApplyForce(float xAcceleration,float yAcceleration)
+   {
+      CalculateSpeed(xAcceleration, yAcceleration);
+      Move();
+   }
 
+   void CalculateSpeed(float xAcceleration,float yAcceleration)
+   {
+      xAcceleration *= Time.deltaTime * config.xAcceleration;
+      yAcceleration *= Time.deltaTime * config.yAcceleration;
+      currentSpeed.x += xAcceleration;
+      currentSpeed.y += yAcceleration;
+      currentSpeed.z = 0;
+      currentSpeed.x = Mathf.Clamp(currentSpeed.x, -config.maxSpeedX, config.maxSpeedX);
+      currentSpeed.y = Mathf.Clamp(currentSpeed.y, -config.maxSpeedY, config.maxSpeedY);
+   }
+
+   void Move()
+   {
+      position = mTrtansform.position;
+      position += currentSpeed * Time.deltaTime;
+      
+   }
    #endregion
 
    #region Coroutines
